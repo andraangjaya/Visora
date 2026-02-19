16e96efa20f69d6926c59753dc3930cfab0c65c6
andra <andrabaktiangjaya@gmail.com>
2026-02-19 11:39:58 +0800

    Added middleware and view components directory

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,5 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title></title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
-</body>
-</html>
+<x-layout>
+    <h1 class="text-2xl font-bold">Dashboard</h1>
+</x-layout>
+
+
