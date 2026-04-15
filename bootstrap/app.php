4af20a282115a8147633b2c1720843200cb5a3b9
andra <andrabaktiangjaya@gmail.com>
2026-04-15 22:13:16 +0800

    Added exception handler for API response

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -21,5 +23,18 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => class_basename($e->getModel()) . ' not found'
+            ], 404);
+        });
+
+        $exceptions->render(function (NotFoundHttpException $e, $request) {
+            if ($request->routeIs('products.*')) {
+                return response()->json([
+                    'message' => 'Resource not found'
+                ], 404);
+            }
+            return null;
+        });
     })->create();
